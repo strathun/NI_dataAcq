0553d508524e442421511407576275602bb38127
Hunter Strathman <hstrathman@gmail.com>
2020-07-28 16:31:33 -0600

    Fix saving function. Was a problem with obj. <- removed this bit.

--- datAcqTotal.m
+++ datAcqTotal.m
@@ -1,13 +1,18 @@
-%% datAqTotal.m 
+%% datAcqTotal.m 
 % Connects to NI DAQ device and records time domain data for specified data
 % rate and duration. Graphs data with significant delay.
 % This is the initial script in this suite. Calls writeDatafun.m
-
+% datAcqLive should not be used... I don't think...
+% Hardware required:
+%   Noise headstage? 
+%   Noise daughterboard?
+clear
+close all
 %__________________User_Defined_Parameters________________________________%
-sampleRate = 1e6;      % sets sampling rate
-sampleDur  = 00.10;    % sets sampling duration (s)
-frameSize  = 00.01;    % time in (s) per plot in live view
-saveFileNote  = 'testingNewHardware';  % String for filename
+sampleRate = 20e3;      % sets sampling rate
+sampleDur  = 01.00;    % sets sampling duration (s)
+frameSize  = 00.1;    % time in (s) per plot in live view
+saveFileNote  = 'atInput_2Hz';  % String for filename
 %_________________________________________________________________________%
 
 datestamp = datestr(date,29);
@@ -31,9 +36,9 @@ s.Rate = sampleRate;
 s.DurationInSeconds = sampleDur;
 
 % Uses two listeners. One that plots data to give a rough live stream of
-% data, and one that writies data to txt file in small chunks. 
+% data, and one that writes data to txt file in small chunks. 
 lh = addlistener(s,'DataAvailable', @(src,event) plot(event.TimeStamps, event.Data));
-llh = addlistener(s,'DataAvailable', @(src,event)obj.writeDatafun(src,event,fileName));
+llh = addlistener(s,'DataAvailable', @(src,event) writeDatafun(src,event,fileName));
 
 s.NotifyWhenDataAvailableExceeds = sampleRate * frameSize;
 s.startBackground();
@@ -47,4 +52,3 @@ figure
 plot(time, data)
 xlabel('Time (s)')
 ylabel('Voltage')
-ylim([-.3 .3]);
